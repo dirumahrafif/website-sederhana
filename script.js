ff13bc5ba087af2ac62fa03afc5d2658011be7d5
dirumahrafif <69278973+dirumahrafif@users.noreply.github.com>
2021-02-26 06:24:00 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var tombolMenu = document.getElementsByClassName('tombol-menu')[0];
+var menu = document.getElementsByClassName('menu')[0];
+
+tombolMenu.onclick = function() {
+    menu.classList.toggle('active');
+}
+
+menu.onclick = function() {
+    menu.classList.toggle('active');
+}
